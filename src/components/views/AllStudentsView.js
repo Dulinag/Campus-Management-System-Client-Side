c7df4c2c6c70cf72983f7545b294eaff5b912f70
Ian Clarke <ianisian2003@gmail.com>
2024-05-09 22:24:05 -0400

    minor fixes

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import { Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
